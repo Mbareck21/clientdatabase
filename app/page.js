21ddbbb4579c293de328a7d018e98378b2643272
Mbareck21 <missara.lamine@gmail.com>
2024-01-05 18:40:08 -0600

    regsration form is talking to the server

--- app/page.js
+++ app/page.js
@@ -1,19 +1,21 @@
-'use client';
-import { useRouter } from 'next/navigation'
+// 'use client';
+// import { useRouter } from 'next/navigation'
 import ClientsList from "@/components/ClientList";
 import Button from '@mui/material/Button';
 import Stack from '@mui/material/Stack';
 import Box from '@mui/material/Box';
 import Grid from '@mui/material/Grid';
 import { Typography } from '@mui/material';
+import Register from '@/components/RegisterForm';
 
 export default function Home() {
-	const router = useRouter();
+	// const 		router = useRouter();
 	
 
 	return (
 		<Box sx={{ flexGrow: 1 }}>
-			<Typography variant="h3">Home Page</Typography>
+			<Typography variant="h5">Home Page</Typography>
+			<Register />
 		</Box>
 	);
 }
